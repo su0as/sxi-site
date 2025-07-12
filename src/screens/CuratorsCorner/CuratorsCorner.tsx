64f3369be88b5c52b96a3b5f92a0cee9a4cf1c0a
Suhas S <91456565+su0as@users.noreply.github.com>
2025-07-12 10:38:12 +0530

    Updated CuratorsCorner.tsx

--- src/screens/CuratorsCorner/CuratorsCorner.tsx
+++ src/screens/CuratorsCorner/CuratorsCorner.tsx
@@ -17,7 +17,7 @@ interface CuratorsCornerProps {
 export const CuratorsCorner: React.FC<CuratorsCornerProps> = ({ onNavigate }) => {
   return (
     <PageLayout currentPage="curators-corner" onNavigate={onNavigate}>
-      <div className="absolute top-[50px] left-[500px] right-[50px] bottom-[50px] overflow-y-auto">
+      <div className="absolute top-1/2 left-[500px] right-[50px] transform -translate-y-1/2">
         <div className="max-w-3xl text-white font-['Gilroy-SemiBold',sans-serif]">
           <div className="text-center">
             <h1 className="text-4xl font-semibold mb-12" style={{ fontFamily: 'Gilroy-SemiBold, sans-serif' }}>
